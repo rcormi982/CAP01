c8baaa8f54ea995af7b4ec47e4d911b8d16ed397
Rocio <rociocor77@gmail.com>
2023-10-07 16:55:27 +0200

    EJ02

---
+++ EJ02.java
@@ -0,0 +1,7 @@
+public class EJ02 {
+    public static void main(String[] args) {
+        System.out.printf("%s%n","Rocío");
+        System.out.printf("%s%n","Avenida Dolores Rodríguez de Aragón");
+        System.out.printf("%s%n","686272008");
+    }
+}
