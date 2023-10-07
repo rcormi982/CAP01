a3ec2d615ee7e0c5fa41153c6be8172fa2a55763
Rocio <rociocor77@gmail.com>
2023-10-07 16:55:55 +0200

    EJ03

---
+++ EJ03.java
@@ -0,0 +1,13 @@
+public class EJ03 {
+    public static void main(String[] args) {
+        System.out.printf("%-8s\t%s%n", "computer", "ordenador");
+        System.out.printf("%-8s\t%s%n", "student", "alumno");  
+        System.out.printf("%-8s\t%s%n", "cat", "gato");
+        System.out.printf("%-8s\t%s%n", "penguin", "pingüino");
+        System.out.printf("%-8s\t%s%n", "machine", "máquina");
+        System.out.printf("%-8s\t%s%n", "nature", "naturaleza");
+        System.out.printf("%-8s\t%s%n", "light", "luz");
+        System.out.printf("%-8s\t%s%n", "green", "verde");
+        System.out.printf("%-8s\t%s%n", "pyramid", "pirámide");
+    }
+}
