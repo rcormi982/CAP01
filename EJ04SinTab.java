cbba39ac9b940da5a646b0dfc7bf2ac990a5accf
Rocio <rociocor77@gmail.com>
2023-10-07 16:57:57 +0200

    EJ04

---
+++ EJ04SinTab.java
@@ -0,0 +1,12 @@
+public class EJ04SinTab {
+    public static void main(String[] args) {
+        System.out.printf("%-5s  %-6s  %9s  %6s  %s%n", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes");
+        System.out.printf("%-5s  %6s  %9s  %6s  %s%n", "=====", "======", "=========", "======", "=======");
+        System.out.printf("%-5s  %-6s  %-9s  %-6s  %s%n", "PROG", "LM", "LM", "PROG", "FOL");
+        System.out.printf("%-5s  %-6s  %-9s  %-6s  %s%n", "PROG", "LM", "LM", "PROG", "FOL");
+        System.out.printf("%-5s  %-6s  %-9s  %-6s  %s%n", "PROG", "BDATO", "PROG", "PROG", "FOL");
+        System.out.printf("%-5s  %-6s  %-9s  %-6s  %s%n", "SINF", "BDATO", "PROG", "BDATO", "SINF");
+        System.out.printf("%-5s  %-6s  %-9s  %-6s  %s%n", "EIE", "ED", "BDATO", "BDATO", "EIE");
+        System.out.printf("%-5s  %-6s  %-9s  %-6s  %s%n", "SINF", "ED", "BDATO", "ED", "EIE");
+    }
+}
