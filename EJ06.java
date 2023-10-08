ee2df8b493ab206ee6da0c5c74c988b02bb1844e
Rocio <rociocor77@gmail.com>
2023-10-08 16:10:35 +0200

    EJ06

---
+++ EJ06.java
@@ -0,0 +1,9 @@
+public class EJ06 {
+    public static void main(String[] args) {
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+        System.out.println("*********");
+    }
+}
