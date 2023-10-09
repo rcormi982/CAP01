e48826fe79b3942c2f0e693d7459740162882ae8
Rocio <rociocor77@gmail.com>
2023-10-09 10:17:46 +0200

    EJ09

---
+++ EJ09.java
@@ -0,0 +1,22 @@
+public class EJ09 {
+    public static void main(String[] args) {
+            String cuadrado = "\u1D127";
+            System.out.println("" +cuadrado + cuadrado + cuadrado + cuadrado + cuadrado +cuadrado +cuadrado +cuadrado +cuadrado +cuadrado +cuadrado);
+            System.out.println("" +cuadrado + cuadrado + cuadrado + cuadrado + cuadrado +cuadrado +cuadrado +cuadrado +cuadrado +cuadrado +cuadrado);
+            System.out.println("" +cuadrado + cuadrado + cuadrado);
+            System.out.println("" +cuadrado + cuadrado + cuadrado);
+            System.out.println("" +cuadrado + cuadrado + cuadrado);
+            System.out.println("" +cuadrado + cuadrado + cuadrado);
+            System.out.println("" +cuadrado + cuadrado + cuadrado);
+            System.out.println("" +cuadrado + cuadrado + cuadrado + cuadrado + cuadrado +cuadrado +cuadrado +cuadrado +cuadrado +cuadrado +cuadrado);
+            System.out.println("" +cuadrado + cuadrado + cuadrado + cuadrado + cuadrado +cuadrado +cuadrado +cuadrado +cuadrado +cuadrado +cuadrado);
+            System.out.printf("%19s%n", "" +cuadrado + cuadrado + cuadrado);
+            System.out.printf("%19s%n", "" +cuadrado + cuadrado + cuadrado);
+            System.out.printf("%19s%n", "" +cuadrado + cuadrado + cuadrado);
+            System.out.printf("%19s%n", "" +cuadrado + cuadrado + cuadrado);
+            System.out.printf("%19s%n", "" +cuadrado + cuadrado + cuadrado);
+            System.out.println("" +cuadrado + cuadrado + cuadrado + cuadrado + cuadrado +cuadrado +cuadrado +cuadrado +cuadrado +cuadrado +cuadrado);
+            System.out.println("" +cuadrado + cuadrado + cuadrado + cuadrado + cuadrado +cuadrado +cuadrado +cuadrado +cuadrado +cuadrado +cuadrado);
+    
+    }
+}
