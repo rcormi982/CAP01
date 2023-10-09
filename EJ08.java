469e176e0320156c6347d3ff4e4a8c1523173503
Rocio <rociocor77@gmail.com>
2023-10-09 10:17:11 +0200

    EJ08

---
+++ EJ08.java
@@ -0,0 +1,9 @@
+public class EJ08 {
+    public static void main(String[] args) {
+        System.out.println("*********");
+        System.out.println(" *     *");
+        System.out.println("  *   *");
+        System.out.println("   * *");
+        System.out.println("    *");
+    }
+}
